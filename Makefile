a0a64b3133c0c8d7d083b763a35f4c23d5eec4c3
Matthias Wenzel <acab@mazzoo.de>
2010-07-15 21:34:23 +0200

    network code. first live watching the gargoyle dance at night seems to work.

--- Makefile
+++ Makefile
@@ -1,3 +1,6 @@
 CFLAGS=-Wall -O2 -ggdb
 LDFLAGS=-lSDL -lSDL_image
 all:puertoplayer
+
+clean:
+	rm -rf puertoplayer
--- puertoplayer.c
+++ puertoplayer.c
@@ -23,6 +23,9 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <errno.h>
 
 #include <SDL/SDL.h>
 #include <SDL/SDL_image.h>
@@ -31,17 +34,34 @@
 int winx = 640;
 int winy = 476;
 
-SDL_Surface * s = NULL; /* colored rectangles / windows */
-SDL_Surface * p = NULL; /* picture of the frontside of the building */
-
 #define HERTIE_GIESING "hertie_640x476.png"
+#define ICON_LOVE      "i_can_prit_love.png"
+
+#define ACAB_IP      (  127  \
+                    |(    0  \
+               << 8)|(    0  \
+               <<16)|(    1  \
+               <<24))
+
+int a; /* socket to acab */
+
+SDL_Surface * s    = NULL; /* colored rectangles / windows */
+SDL_Surface * p    = NULL; /* picture of the frontside of the building */
+SDL_Surface * icon = NULL; /* icon of love */
+
+int mode = 0; /* 0: play local argv[1] file
+	       * 1: connect live to gigargoyle @ puerto giesing
+	       */
 
 #define PUERTO_X 24
 #define PUERTO_Y 4
 
 uint8_t frame[PUERTO_X * PUERTO_Y * 3];
 
-//#define TEST 1
+uint8_t buf[4096];
+
+SDL_Event e;
+
 
 void refresh_frame(void)
 {
@@ -52,19 +72,9 @@ void refresh_frame(void)
 	int off_x = 55;
 	int off_y = 50;
 
-#if 0
-	pixel.w = winx / PUERTO_X;
-	pixel.h = winy / PUERTO_Y;
-#else
 	pixel.w = 21;
 	pixel.h = 69;
-#endif
 
-#ifdef TEST
-	static int col = 0;
-	if (col) col = 0;
-	else     col = 1;
-#endif
 	for (ix=0; ix < PUERTO_X; ix++)
 	{
 		pixel.x = pixel.w * ix + off_x;
@@ -73,15 +83,9 @@ void refresh_frame(void)
 			pixel.y = pixel.h * iy + off_y;
 			SDL_FillRect(s, &pixel, SDL_MapRGB(
 			                               s->format,
-#ifdef TEST
-						       (ix+iy+col)%2 ? 0 : 0xff,
-						       (ix+iy+col)%2 ? 0 : 0xff,
-						       (ix+iy)%2 ? 0 : 0xff
-#else
 			                               frame[(ix + iy * PUERTO_X) * 3 + 0],
 			                               frame[(ix + iy * PUERTO_X) * 3 + 1],
 			                               frame[(ix + iy * PUERTO_X) * 3 + 2]
-#endif
 						       ));
 		}
 	}
@@ -98,44 +102,81 @@ void load_hertie_giesing(void)
 		      HERTIE_GIESING, SDL_GetError());
 		exit(1);
 	}
-#if 0
-	int ret;
-	ret = SDL_SetAlpha(p, SDL_SRCALPHA, 128);
-	if (ret)
+	icon = IMG_Load(ICON_LOVE);
+	if (!icon)
 	{
-		printf("ERROR: SDL_SetAlpha(): %s\n",
-		      SDL_GetError());
+		printf("ERROR: IMG_Load(%s): %s\n",
+		      ICON_LOVE, SDL_GetError());
 		exit(1);
 	}
-	ret = SDL_SetColorKey(p, SDL_SRCCOLORKEY, SDL_MapRGB(s->format, 0xff, 0xff, 0xff));
-	if (ret)
-	{
-		printf("ERROR: SDL_SetColorKey(): %s\n",
-		      SDL_GetError());
-		exit(1);
-	}
-#endif
 }
+
+void init_socket(void)
+{
+	int ret;
+	struct sockaddr_in sa;
+	a = socket(AF_INET, SOCK_STREAM, 0);
+	if (a < 0)
+	{
+		printf("ERROR: connect(): %s\n", strerror(errno));
+		exit(1);
+	}
+	memset(&sa, 0, sizeof(sa));
+	sa.sin_family        = AF_INET;
+	sa.sin_port          = htons(80);
+	sa.sin_addr.s_addr   = ACAB_IP;
+	ret = connect(a, (struct sockaddr *) &sa, sizeof(sa));
+	if (ret < 0)
+	{
+		printf("ERROR: connect(): %s\n", strerror(errno));
+		exit(1);
+	}
+}
+
 
 int main(int argc, char ** argv)
 {
 	int ret;
-	int f;
+	int f = -1;
 
-	f = open(argv[1], O_RDONLY);
+	if (argv[1])
+		f = open(argv[1], O_RDONLY);
+	else{
+		mode = 1;
+		init_socket();
+	}
+
 	ret = SDL_Init(SDL_INIT_VIDEO);
 	atexit(SDL_Quit);
 
 	load_hertie_giesing();
 	s = SDL_SetVideoMode(winx, winy, 24, SDL_HWSURFACE);
 
-	while (read(f, frame, PUERTO_X * PUERTO_Y * 3) == PUERTO_X * PUERTO_Y * 3)
+	SDL_WM_SetCaption( "puertoplayer - ACAB - all colours are beautiful", "ACAB" );
+	SDL_WM_SetIcon(icon, NULL);
+
+	if (mode)
 	{
-		refresh_frame();
-		SDL_Delay(30);
+		while (read(a, frame, PUERTO_X * PUERTO_Y * 3) == PUERTO_X * PUERTO_Y * 3)
+		{
+			refresh_frame();
+			SDL_Delay(30);
+			SDL_PollEvent(&e );
+			if( e.type == SDL_QUIT )
+				exit(0);
+		}
+	}else{
+		while (read(f, frame, PUERTO_X * PUERTO_Y * 3) == PUERTO_X * PUERTO_Y * 3)
+		{
+			refresh_frame();
+			SDL_Delay(30);
+			SDL_PollEvent(&e );
+			if( e.type == SDL_QUIT )
+				exit(0);
+		}
 	}
 
-	SDL_Delay(3000);
+	SDL_Delay(2342);
 
 	SDL_Quit();
 	return 0;
